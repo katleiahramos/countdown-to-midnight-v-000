e5593abceaf64df4b76074b757786d213bc19353
Katleiah <katleiahramos@gmail.com>
2018-03-06 16:37:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,8 +11,8 @@ def countdown(countdown_number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  number = countdown_number(countdown_number)
+def countdown_with_sleep(countdown_number)
+  number = countdown_number
     while number > 0
       sleep(1)
       puts "#{number} SECOND(S)!"
