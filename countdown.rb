dc6598f760209270c520ba28c3a180ecbf18b914
Katleiah <katleiahramos@gmail.com>
2018-03-06 16:31:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,8 +6,9 @@ def countdown(countdown_number)
   number = countdown_number
     while number > 0
       puts "#{number} SECOND(S)!"
-      number -= 1
       binding.pry
+      number -= 1
+      
     end
   puts "HAPPY NEW YEAR!"
 end
